627207e32e4da052d10e8be7b64a1ce705b4856a
Diego P. Costa <diegussdev@gmail.com>
2021-09-11 10:07:56 -0300

    Criando prórpio framework de testes

---
+++ aula_01/DiscountCalculatorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+class DiscountCalculatorTest
+{
+
+    public function ShoulApply_WhenValueIsAboveTheMinimumTest()
+    {
+        $discountCalculator = new DiscountCalculator();
+
+        $totalValue = 130;
+        $totalWithDiscount = $discountCalculator->apply($totalValue);
+
+        $expectedValue = 110;
+        $this->assertEquals($expectedValue, $totalWithDiscount);
+    }
+
+    public function ShoulNotApply_WhenValueIsBellowTheMinimumTest()
+    {
+        $discountCalculator = new DiscountCalculator();
+
+        $totalValue = 90;
+        $totalWithDiscount = $discountCalculator->apply($totalValue);
+
+        $expectedValue = 90;
+        $this->assertEquals($expectedValue, $totalWithDiscount);
+    }
+
+    public function assertEquals($expectedValue, $actualValue)
+    {
+        if ($expectedValue !== $actualValue) {
+            $message = "Expected: {$expectedValue} but got {$actualValue}";
+            throw new \Exception($message);
+        }
+
+        echo "Test passed! \n";
+    }
+}
---
+++ aula_01/index.php
@@ -0,0 +1,7 @@
+<?php
+
+include 'autoloader.php';
+
+$discountCalculator = new DiscountCalculator();
+
+echo $discountCalculator->apply(130) . "\n";
