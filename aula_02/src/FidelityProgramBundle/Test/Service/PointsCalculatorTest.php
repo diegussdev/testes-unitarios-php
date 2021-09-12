d6e3a15863e8da17670c6a6042d8ced1adf8bc25
Diego P. Costa <diegussdev@gmail.com>
2021-09-12 10:32:30 -0300

    Entendendo relatório de cobertura de testes

---
+++ aula_02/src/FidelityProgramBundle/Test/Service/PointsCalculatorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace FidelityProgramBundle\Test\Service;
+
+use FidelityProgramBundle\Service\PointsCalculator;
+use PHPUnit\Framework\TestCase;
+
+class PointsCalculatorTest extends TestCase
+{
+
+    /**
+     * @test
+     * @dataProvider valueDataProvider
+     */
+    public function testPointsReceive($value, $expectedPoints)
+    {
+        $pointsCalculator = new PointsCalculator();
+        $points = $pointsCalculator->calculatePointsToReceive($value);
+
+        $this->assertEquals($expectedPoints, $points);
+    }
+
+    public function valueDataProvider()
+    {
+        return [
+            [30, 0],
+            [55, 1100],
+            [75, 2250],
+            [105, 5250],
+        ];
+    }
+}
