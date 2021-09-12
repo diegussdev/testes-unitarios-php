fcd681bf7f617654524bfd737ff3daaf3635c792
Diego P. Costa <diegussdev@gmail.com>
2021-09-12 10:46:39 -0300

    Criando primeiro o teste

--- aula_02/src/OrderBundle/Test/Service/CustomerCategoryServiceTest.php
+++ aula_02/src/OrderBundle/Test/Service/CustomerCategoryServiceTest.php
@@ -1,10 +1,23 @@
 <?php
 
-namespace OrderBundle\Service\Test;
+namespace OrderBundle\Test\Service;
 
+use OrderBundle\Entity\Customer;
+use OrderBundle\Service\CustomerCategoryService;
 use PHPUnit\Framework\TestCase;
 
 class CustomerCategoryServiceTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function customerShouldBeNewUser()
+    {
+        $customerCategoryService = new CustomerCategoryService();
 
+        $customer = new Customer();
+        $usageCategory = $customerCategoryService->getUsageCategory($customer);
+
+        $this->assertEquals('new-user', $usageCategory);
+    }
 }
