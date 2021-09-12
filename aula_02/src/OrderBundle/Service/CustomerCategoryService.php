aa6a0c8ab496d83bf20c99d62d8e937b6bb729d7
Diego P. Costa <diegussdev@gmail.com>
2021-09-12 13:29:05 -0300

    Finalizando implementação de TDD

--- aula_02/src/OrderBundle/Service/CustomerCategoryService.php
+++ aula_02/src/OrderBundle/Service/CustomerCategoryService.php
@@ -7,9 +7,23 @@ use OrderBundle\Entity\Customer;
 class CustomerCategoryService
 {
     const CATEGORY_NEW_USER = 'new-user';
+    const CATEGORY_LIGHT_USER = 'light-user';
+    const CATEGORY_MEDIUM_USER = 'medium-user';
+    const CATEGORY_HEAVY_USER = 'heavy-user';
+
+    private $categories;
+
+    public function addCategory(CustomerCategoryInterface $category)
+    {
+        $this->categories[] = $category;
+    }
 
     public function getUsageCategory(Customer $customer)
     {
-        return self::CATEGORY_NEW_USER;
+        foreach ($this->categories as $category) {
+            if ($category->isEligible($customer)) {
+                return $category->getCustomerCategoryName();
+            }
+        }
     }
 }
---
+++ aula_02/src/OrderBundle/Service/HeavyUserCategory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OrderBundle\Service;
+
+use OrderBundle\Entity\Customer;
+
+class HeavyUserCategory implements CustomerCategoryInterface
+{
+    public function isEligible(Customer $customer)
+    {
+        return $customer->getTotalOrders() >= 50
+        && $customer->getTotalRatings() >= 10
+        && $customer->getTotalRecommendations() >= 5;
+    }
+
+    public function getCustomerCategoryName()
+    {
+        return CustomerCategoryService::CATEGORY_HEAVY_USER;
+    }
+}
